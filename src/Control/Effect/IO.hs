c996f19fbbf2d1ae2be72b25b790b18f15e9d039
Zhixuan Yang <s.yang20@imperial.ac.uk>
2023-12-19 21:48:20 +0000

    Finish refactoring in src/

--- src/Control/Effect/IO.hs
+++ src/Control/Effect/IO.hs
@@ -2,34 +2,40 @@
 
 module Control.Effect.IO where
 
-import Control.Effect
+import Control.Effects
+import Control.Handler
+import Control.Family.AlgScp
 import Data.List.Kind
 import Data.Functor.Composes
 
-data GetLine k  = GetLine (String -> k) deriving Functor
+data GetLine' k  = GetLine (String -> k) deriving Functor
+
+type GetLine = Algebraic GetLine'
 
 getLine :: Members '[GetLine] sig => Prog sig String
-getLine = injCall (Alg (GetLine return))
+getLine = injCall (Algebraic (GetLine return))
 
 
-data PutStrLn k = PutStrLn String k     deriving Functor
+data PutStrLn' k = PutStrLn String k     deriving Functor
+
+type PutStrLn = Algebraic PutStrLn'
 
 putStrLn :: Members '[PutStrLn] sig => String -> Prog sig ()
-putStrLn str = injCall (Alg (PutStrLn str (return ())))
+putStrLn str = injCall (Algebraic (PutStrLn str (return ())))
 
 
 algIO :: Effs [GetLine, PutStrLn] IO a -> IO a
 algIO eff
-  | Just (Alg (GetLine k))    <- prj eff =
+  | Just (Algebraic (GetLine k))    <- prj eff =
       do str <- Prelude.getLine
          return (k str)
-  | Just (Alg (PutStrLn str k)) <- prj eff =
+  | Just (Algebraic (PutStrLn str k)) <- prj eff =
       do Prelude.putStrLn str
          return k
 
 algPutStrLn :: Effs '[PutStrLn] IO a -> IO a
 algPutStrLn eff
-  | Just (Alg (PutStrLn str k)) <- prj eff =
+  | Just (Algebraic (PutStrLn str k)) <- prj eff =
       do Prelude.putStrLn str
          return k
 
@@ -37,13 +43,14 @@ evalIO :: Prog [GetLine, PutStrLn] a -> IO a
 evalIO = eval algIO
 
 handleIO
-  :: forall ieffs oeffs fs a xeffs
+  :: forall ieffs oeffs fs a xeffs fam
   . ( Append ieffs (xeffs :\\ ieffs)
     , Injects oeffs xeffs
     , Injects (xeffs :\\ ieffs) xeffs
     , Recompose fs
+    , fam (Effs '[GetLine, PutStrLn])
     , xeffs ~ '[GetLine, PutStrLn] )
-  => Handler ieffs oeffs fs
+  => Handler ieffs oeffs fs fam
   -> Prog (ieffs `Union` xeffs) a -> IO (Composes fs a)
 handleIO = handleWith algIO
 
