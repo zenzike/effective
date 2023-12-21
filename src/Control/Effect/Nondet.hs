2b4c608cdf3aa5b0deb2f98a316409ceada5f175
Nicolas Wu <nicolas.wu@gmail.com>
2023-12-21 23:18:28 +0000

    Add depth bound search

--- src/Control/Effect/Nondet.hs
+++ src/Control/Effect/Nondet.hs
@@ -8,20 +8,21 @@ import Prelude hiding (or)
 
 import Control.Monad.Trans.Class ( MonadTrans(..) )
 import Control.Effect
-import Control.Monad.Trans.List
+import Control.Monad.Trans.List ( runListT', ListT(..) )
+import Control.Monad.Trans.State ( StateT(..), withStateT, get, put  )
 import Control.Applicative ( Alternative(empty, (<|>)) )
 
 
 data Stop a where
   Stop :: Stop a
-  deriving Functor
+  deriving (Functor, Show)
 
 stop :: Members '[Stop] sig => Prog sig a
 stop  = injCall (Alg Stop)
 
 data Or a where
   Or :: a -> a -> Or a
-  deriving Functor
+  deriving (Functor, Show)
 
 or :: Members '[Or] sig => Prog sig a -> Prog sig a -> Prog sig a
 or x y = injCall (Alg (Or x y))
@@ -36,6 +37,11 @@ instance (Members [Or, Stop] sig) => Alternative (Prog sig) where
 
 select :: Members [Or, Stop] sig => [a] -> Prog sig a
 select = foldr (or . return) stop
+
+selects :: [a] -> Prog' [Or, Stop] (a, [a])
+selects []      =  empty
+selects (x:xs)  =  return (x, xs)  <|>  do  (y, ys) <- selects xs
+                                            return (y, x:ys)
 
 nondetAlg
   :: (MonadTrans t, Alternative (t m) , Monad m)
@@ -60,11 +66,37 @@ nondet = handler runListT' nondetAlg nondetFwd
 -- Example: Backtracking (and Culling?)
 data Once a where
   Once :: a -> Once a
-  deriving Functor
+  deriving (Functor, Show)
 
 once
   :: Member Once sig => Prog sig a -> Prog sig a
 once p = injCall (Scp (Once (fmap return p)))
+
+data Search a where
+  Search :: a -> Search a
+  deriving Functor
+
+search :: Member Search sig => Prog sig a -> Prog sig a
+search p = injCall (Scp (Search (fmap return p)))
+
+data Depth a where
+  Depth :: Int -> a -> Depth a
+  deriving Functor
+
+depth :: Member Depth sig => Int -> Prog sig a -> Prog sig a
+depth d p = injCall (Scp (Depth d (fmap return p)))
+
+data DBS a where
+  DBS :: Int -> a -> DBS a
+  deriving Functor
+
+dbs :: Member DBS sig => Int -> Prog sig a -> Prog sig a
+dbs d p = injCall (Scp (DBS d (fmap return p)))
+
+-- searchOnce :: Handler '[Search] '[Once] '[]
+-- searchOnce = interpret undefined
+
+-- searchWith strategy = interpret ... strategy ...
 
 -- Everything can be handled together. Here is the non-modular way
 -- list :: (Member (Or) sig, Member (Stop) sig, Member (Once) sig) => Prog sig a -> [a]
@@ -108,6 +140,34 @@ backtrackAlg' = joinAlg nondetAlg backtrackOnceAlg
 -- TODO: The alternative with monad transformers is painful.
 -- TODO: this becomes interesting when different search strategies are used
 
+-- backtrackDepthAlg
+--   :: Monad m
+--   => (forall x . oeff m x -> m x)
+--   -> (forall x . Effs '[Stop, Or, Depth] (ReaderT Int (ListT m)) x -> ReaderT Int (ListT m) x)
+-- backtrackDepthAlg oalg op
+--   | Just (Alg Stop)        <- prj op = ReaderT (const empty)
+--   | Just (Alg (Or x y))    <- prj op = ReaderT (\d -> if d == 0
+--                                                         then empty
+--                                                         else _)
+--   | Just (Scp (Depth d p)) <- prj op =
+--       ReaderT (\d' -> ListT (do mx <- runListT (runReaderT p d)
+--                                 case mx of
+--                                   Nothing       -> return Nothing
+--                                   Just (x, mxs) -> return (Just (x, mxs))
+--       ))
+
+backtrackDepthAlg
+  :: Monad m
+  => (forall x . oeff m x -> m x)
+  -> (forall x . Effs '[Stop, Or, Depth] (StateT Int (ListT m)) x -> StateT Int (ListT m) x)
+backtrackDepthAlg oalg op
+  | Just (Alg Stop)     <- prj op = empty
+  | Just (Alg (Or x y)) <- prj op =
+      do d <- get
+         if d == 0
+           then empty
+           else (put (d - 1) >> pure x) <|> (put (d - 1) >> pure y)
+  | Just (Scp (Depth d p)) <- prj op = withStateT (const d) p
 
 backtrackFwd
   :: (Monad m)
@@ -119,3 +179,7 @@ backtrackFwd alg (Effs effs)   = backtrackFwd (alg . Effs) effs
 
 backtrack :: Handler [Stop, Or, Once] '[] '[[]]
 backtrack = handler runListT' backtrackAlg' backtrackFwd
+
+
+-- searchWith :: Handler '[Stop, Or, Once, Search] '[] '[[]]
+-- searchWith = handler runListT' backtrackAlg' backtrackFwd
