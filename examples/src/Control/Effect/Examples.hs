a1c0a1b0ba19c7211ee8569c7cc27a920a09b268
Daniel Trowbridge <danieljtrowbridge@pm.me>
2026-02-27 14:47:36 +0000

    feat: add minimal scoped effect example (Identity)

---
+++ examples/src/Control/Effect/Examples.hs
@@ -0,0 +1,13 @@
+-- |
+-- Module: Control.Effect.Examples
+-- Description: Examples of effects and handlers
+-- License: BSD-3-Clause
+-- Maintainer: Nicolas Wu
+-- Stability: experimental
+module Control.Effect.Examples
+  ( -- * Scoped effects
+    module Control.Effect.Examples.Scoped.Identity,
+  )
+where
+
+import Control.Effect.Examples.Scoped.Identity
---
+++ examples/src/Control/Effect/Examples/Scoped/Identity.hs
@@ -0,0 +1,51 @@
+-- |
+-- Module: Control.Effect.Examples.Scoped.Identity
+-- Description: A scoped effect and handler which just runs an input program.
+-- License: BSD-3-Clause
+-- Maintainer: Nicolas Wu
+-- Stability: experimental
+module Control.Effect.Examples.Scoped.Identity
+  ( -- * Syntax
+
+    -- ** Operation
+    identity,
+
+    -- * Signature
+    Identity,
+    Identity_ (..),
+
+    -- * Semantics
+
+    -- ** Handler
+    runIdentity,
+
+    -- ** Algebra
+    identityAT,
+  )
+where
+
+import Control.Effect hiding (Identity, identity)
+import Control.Effect.Family.Scoped
+
+newtype Identity_ k where
+  Identity :: k -> Identity_ k
+  deriving (Functor)
+
+type Identity = Scp Identity_
+
+{-# INLINE identity #-}
+identity :: forall sig a. (Member Identity sig) => Prog sig a -> Prog sig a
+identity p = call (Scp (Identity p))
+
+runIdentity :: Handler '[Identity] '[] '[] a a
+runIdentity = handler' id identityAlg
+
+identityAT :: AlgTrans '[Identity] '[] '[] Monad
+identityAT = AlgTrans identityAlg
+
+identityAlg ::
+  (Monad m) =>
+  (forall x. oeff m x -> m x) ->
+  (forall x. Effs '[Identity] m x -> m x)
+identityAlg _ eff
+  | Just (Scp (Identity p)) <- prj eff = p
